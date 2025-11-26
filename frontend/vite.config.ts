1a405ed68de36206b56c56678eb1cf8765a1aca5
Ajay <ajayrajnegi111@gmail.com>
2025-11-26 21:05:11 +0530

    Code cleanup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
   server: {
-    allowedHosts: [
-      '.ngrok-free.app'
-    ],
-    host: true 
+    allowedHosts: [".ngrok-free.app"],
+    host: true,
   },
-  
+
   plugins: [react(), tsconfigPaths()],
-})
+});
